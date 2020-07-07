28017b15eb31df97581f7a99c15a084f0a69b366
origin <863007116@qq.com>
2020-07-07 19:58:59 +0800

    files skeleton modify

---
+++ test/test0-files check.py
@@ -0,0 +1,32 @@
+import os
+
+PATH = 'E://PyCharm_project//Pytorch-UNet//data'
+
+
+# 数据整理（图片&标签匹配）
+def list_dir(folder1, folder2):
+    list1 = os.listdir(PATH + folder1)
+    list2 = os.listdir(PATH + folder2)
+    print(list1)
+    print(list2)
+
+    i = 0
+    while i < len(list1):
+        tmp1 = list1[i].split('.')[0]
+        tmp2 = list2[i].split('.')[0]
+
+        if tmp1 == tmp2:
+            i = i + 1
+        else:
+            print(list1[i] + "---" + list2[i])
+            os.remove(PATH + folder2 + '//' + list2[i])
+            list2.pop(i)
+
+    while i < len(list2):
+        print("list1 over ---" + list2[i])
+        os.remove(PATH + folder2 + '//' + list2[i])
+        list2.pop(i)
+
+
+if __name__ == '__main__':
+    list_dir('//masks', '//imgs')
