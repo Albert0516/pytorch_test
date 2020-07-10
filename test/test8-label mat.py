f9f55aa3dcc033ddfac65caad8287851c942c581
origin <863007116@qq.com>
2020-07-10 20:31:03 +0800

    files add

--- test/test8-label mat.py
+++ test/test8-label mat.py
@@ -5,25 +5,39 @@ import matplotlib.pyplot as plt
 
 file_path = 'E://BaiduNetdiskDownload//Face-Occlusion-Detect//data//cofw//COFW_train.mat'
 
-data = h5py.File(file_path, 'r')
-print(list(data.keys()))
+data = h5py.File(file_path, 'r')            # ['#refs#', 'IsTr', 'bboxesTr', 'phisTr']
+images = np.transpose(data['IsTr'])         # (1345, 1)
+labels = np.transpose(data['phisTr'])       # (1345, 87)
 
-# images = np.transpose(data['IsTr'])
-# print(images.shape)
-# plt.imshow(images[0])
-# plt.show()
+plt.ion()
+for index in range(len(images)):
+    # 理解！
+    image = np.transpose(data[images[index][0]])
+    label = labels[index]
+    x, x_occluded = [], []
+    y, y_occluded = [], []
+    for i in range(58, len(label)):
+        if label[i] == 1:
+            x_occluded.append(label[i-58])
+            y_occluded.append(label[i-29])
+        else:
+            x.append(label[i-58])
+            y.append(label[i-29])
 
-phisTr = np.transpose(data['phisTr'])
-index = 10
-x = np.array(phisTr[index][0:29])
-y = np.array(phisTr[index][29:58])
-label = np.array(phisTr[index][58:])
+    x = np.array(x)
+    y = np.array(y)
+    plt.scatter(x, y, marker='*', color='b')
 
-plt.scatter(x, y, marker='*', color='r')
-plt.show()
-# for label in phisTr:
-#     print(label)
-#     time.sleep(1)
+    if len(x_occluded) > 0:
+        x_occluded = np.array(x_occluded)
+        y_occluded = np.array(y_occluded)
+        plt.scatter(x_occluded, y_occluded, marker='*', color='r')
+
+    plt.imshow(image)
+    plt.draw()
+    plt.pause(0.5)
+    plt.clf()
+
 
 
 
--- test/test9-view dataset.py
+++ test/test9-view dataset.py
@@ -1,9 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-data = np.load('D://AI调研//比赛&数据集//youtube_faces_with_keypoints_full_1//Aaron_Eckhart_1.npz')
-print(data.files)
-image = data['colorImages']
-print(image.shape)
+# os.listdir('D://AI调研//比赛&数据集//youtube_faces_with_keypoints_full_1//')
+path = 'D://AI调研//比赛&数据集//youtube_faces_with_keypoints_full_1//Adrian_Nastase_4.npz'
+# 数据格式-['colorImages', 'boundingBox', 'landmarks2D', 'landmarks3D']
+data = np.load(path)
 
-# plt.imshow(np.transpose(image))
+# (179, 144, 3, num_frames) -> (num_frames, 179, 144, 3)
+images = data['colorImages'].transpose((3, 0, 1, 2))
+box = data['boundingBox'].transpose((2, 0, 1))          # (num_frames, 4, 2)
+marks2d = data['landmarks2D'].transpose(2, 0, 1)        # (num_frames, 68, 2)
+marks3d = data['landmarks3D'].transpose(2, 0, 1)        # (num_frames, 68, 3)
+
+# plt.ion()
+# for i in range(len(images)):
+#     image = images[i]
+#     x = marks2d[i, :, 0]
+#     y = marks2d[i, :, 1]
+#
+#     plt.imshow(image)
+#     plt.scatter(x, y, marker='*', color='r')
+#     plt.draw()
+#     # plt.show()
+#     plt.pause(0.5)
+#     plt.clf()
+
